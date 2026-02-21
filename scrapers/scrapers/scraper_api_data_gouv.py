738b61855689aee8d7a79955adc56d51b0ddf395
simondac <dacosta.simon@protonmail.com>
2026-02-21 22:22:43 +0100

    ajout écriture CSV + ajout commentaires

--- scrapers/scrapers/scraper_api_data_gouv.py
+++ scrapers/scrapers/scraper_api_data_gouv.py
@@ -1,7 +1,15 @@
+# TODO
+"""
+TODO: écriture du CSV en sortie sur le bucket S3
+TODO: logique de développement 
+"""
+
+
 # Packages
 import requests
 import json
 import pandas as pd
+
 
 # Création du dictionnaire de correspondance donnée --> id_ressource
 dict_corresp_data_id = {
@@ -12,16 +20,19 @@ dict_corresp_data_id = {
     "maires": "2876a346-d50c-4911-934e-19ee07b0e503"
 }
 
+
+# Paramètres API
 nb_pages = 2
 page_size = 200
-data = "deputees"
+source_donnees = "deputees"
 
 
+# Fonction de récupération des données via l'API
 def recuperer_data_api(nb_pages):
     df_final = pd.DataFrame()
 
     for page in range(1, nb_pages + 1):
-        pattern_url = f"https://tabular-api.data.gouv.fr/api/resources/{dict_corresp_data_id[data]}/data/?page={page}&page_size={page_size}"
+        pattern_url = f"https://tabular-api.data.gouv.fr/api/resources/{dict_corresp_data_id[source_donnees]}/data/?page={page}&page_size={page_size}"
         
         # Détermination de l'URL en fonction de la page et de la taille de page
         URL_PAGE = pattern_url.format(page=page, page_size=page_size)
@@ -45,8 +56,11 @@ def recuperer_data_api(nb_pages):
         df_final = pd.concat([df_final, resultat_temp], ignore_index=True)
 
         return df_final
-    
 
+
+# Lancement de la fonction de récupération des données
 df_final = recuperer_data_api(nb_pages=2)
 
-print(df_final.head)
+
+# Ecriture des résultats dans un CSV dans /data/raw/
+df_final.to_csv(f"data/raw/{source_donnees}.csv", index=False)
