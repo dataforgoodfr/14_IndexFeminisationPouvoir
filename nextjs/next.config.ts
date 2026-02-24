9f94af508f23368105003e05379d5788d6ccc91d
caterina <caterina.mennito@gmail.com>
2026-02-24 22:36:27 +0100

    add basePath

--- nextjs/next.config.ts
+++ nextjs/next.config.ts
@@ -5,6 +5,8 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
   output: 'export',
   trailingSlash: true,
+  basePath: '/14_IndexFeminisationPouvoir',
+  assetPrefix: '/14_IndexFeminisationPouvoir',
   images: {
     unoptimized: true,
   },
