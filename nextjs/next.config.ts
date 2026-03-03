a6aaeec4116bd5fe16fd3310ea32cca9a9336ae9
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2026-03-03 20:57:27 +0100

    Configuration de la CI pour les PRs, et mise à jour à Node LTS (#16)

--- nextjs/next.config.ts
+++ nextjs/next.config.ts
@@ -6,6 +6,8 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
   output: "export",
   trailingSlash: true,
+  // This should be injected by CI, but currently the next.config.ts extension isn't supported
+  // cf https://github.com/actions/configure-pages/issues/177
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   images: {
     unoptimized: true,
