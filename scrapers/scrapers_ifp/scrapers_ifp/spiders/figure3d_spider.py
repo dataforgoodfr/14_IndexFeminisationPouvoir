3318a3ece9cedb2fc050959277899eac7d74831a
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2026-03-13 18:26:17 +0100

    croisement avec 3c pour récupérer la civilité

--- scrapers/scrapers_ifp/scrapers_ifp/spiders/figure3d_spider.py
+++ scrapers/scrapers_ifp/scrapers_ifp/spiders/figure3d_spider.py
@@ -8,13 +8,31 @@ class Figure3dSpider(scrapy.Spider):
     name = "figure3d"
     description = "Femmes présidant un groupe au Sénat"
 
-    start_urls = ["https://data.senat.fr/data/senateurs/ODSEN_HISTOGROUPES.json"]
+    start_urls = [
+        "https://data.senat.fr/data/senateurs/ODSEN_GENERAL.json",
+        "https://data.senat.fr/data/senateurs/ODSEN_HISTOGROUPES.json",
+    ]
     fonctions = ["Président", "Présidente", "Délégué", "Déléguée"]
 
-    async def parse(self, response: TextResponse):
+    matricule_to_civilite = {}
+
+    async def start(self):
+        yield scrapy.Request(url=self.start_urls[0], callback=self.parse_civilites)
+        yield scrapy.Request(url=self.start_urls[1], callback=self.parse_membres_bureau)
+
+    async def parse_civilites(self, response: TextResponse):
         json_response = response.json()
         results = json_response.get("results", [])
         for result in results:
+            matricule = result.get("Matricule")
+            civilite = result.get("Qualite")
+            self.matricule_to_civilite[matricule] = civilite
+
+    async def parse_membres_bureau(self, response: TextResponse):
+        json_response = response.json()
+        results = json_response.get("results", [])
+        for result in results:
+            matricule = result.get("Matricule")
             nom = result.get("Nom")
             prenom = result.get("Prenom")
             groupe = result.get("Nom_court_du_groupe_politique")
@@ -26,12 +44,18 @@ class Figure3dSpider(scrapy.Spider):
             if fonction not in self.fonctions:
                 continue
 
-            # TODO civilté non disponible, à déterminer à partir d'autres sources du Sénat
+            civilite = self.matricule_to_civilite.get(matricule)
+            if not civilite:
+                self.logger.warning(
+                    f"Matricule {matricule} non trouvé dans les civilités"
+                )
+
             item = Personne(
-                personne_raw_text=f"{prenom} {nom}",
+                personne_raw_text=f"{civilite} {prenom} {nom}",
                 personne_nom_complet=f"{prenom} {nom}",
+                personne_civilite=civilite,
                 personne_prenom=prenom,
                 personne_nom=nom,
                 poste_libelle=groupe,
             )
-            yield item.model_dump()
+            yield item
