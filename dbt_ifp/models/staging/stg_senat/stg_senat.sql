451aff668488e1efb5f0fcc8d9d3e904e2d145a4
simondac <dacosta.simon@protonmail.com>
2026-03-22 22:49:42 +0100

    ajout staging senat

---
+++ dbt_ifp/models/staging/stg_senat/stg_senat.sql
@@ -0,0 +1,16 @@
+SELECT
+    __id as id,
+    "Code du département" as code_departement,
+    "Libellé du département" as libelle_departement,
+    "Code de la collectivité à statut particulier" as code_collectivite_statut_part,
+    "Libellé de la collectivité à statut particulier" as nom_collectivite_statut_part,
+    "Nom de l'élu" as nom_elu,
+    "Prénom de l'élu" as prenom_elu,
+    "Code sexe" as genre,
+    "Date de naissance" as date_naissance,
+    "Code de la catégorie socio-professionnelle" as code_categorie_socprof,
+    "Libellé de la catégorie socio-professionnelle" as nom_categorie_socprof,
+    "Date de début du mandat" as date_debut_mandat,
+    source_scraper
+FROM
+    {{ ref('figure3a_20260312_090553') }}
