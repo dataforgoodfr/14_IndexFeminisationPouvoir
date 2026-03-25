cc9d38f90aeb074de1949127096a1239c495dad3
20k-P <11522296+20k-P@users.noreply.github.com>
2026-03-25 20:11:58 +0100

    feat: update model to read severals csv according to year

--- dbt_ifp/models/staging/stg_figure6b.sql
+++ dbt_ifp/models/staging/stg_figure6b.sql
@@ -1,6 +1,11 @@
+-- stg_figure6b.sql
+{% set start_year = 2026 %}
+{% set current_year = modules.datetime.datetime.now().year %}
+
+{% for year in range(start_year, current_year + 1) %}
 SELECT
-    personne_raw_text,
-    personne_nom_complet,
+    {{ year }} AS year_partition,
+    md5(personne_raw_text || zone_geographique_libelle) AS id,
     personne_civilite,
     personne_prenom,
     personne_nom,
@@ -10,5 +15,7 @@ SELECT
     zone_geographique_libelle,
     zone_geographique_type,
     source_url
-FROM 
-    {{ ref('figure6b_20260320_140724') }}
+FROM
+    {{ ref('figure6b_' ~ year) }}
+{% if not loop.last %} UNION ALL {% endif %}
+{% endfor %}
