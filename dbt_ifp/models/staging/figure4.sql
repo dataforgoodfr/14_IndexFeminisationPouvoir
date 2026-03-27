f68dcc40ce63d76c22ecfa6e62338e9320fa62c0
simondac <dacosta.simon@protonmail.com>
2026-03-27 19:50:16 +0100

    init staging figure4

---
+++ dbt_ifp/models/staging/figure4.sql
@@ -0,0 +1,20 @@
+{% set start_year = 2026 %}
+{% set current_year = modules.datetime.datetime.now().year %}
+
+
+{% for year in range(start_year, current_year + 1) %}
+select
+    {{ year }} as annee_partition,
+    __id as id,
+    "Nom de l'élu" as nom_elu,
+    "Prénom de l'élu" as prenom_elu,
+    "Code sexe" as genre,
+    "Date de naissance" as date_naissance,
+    "Code de la catégorie socio-professionnelle" as code_categorie_socprof,
+    "Libellé de la catégorie socio-professionnelle" as nom_categorie_socprof,
+    "Date de début du mandat" as date_debut_mandat,
+    source_url
+from
+    {{ ref('stg_figure4_' ~ year) }}
+{% if not loop.last %} UNION ALL {% endif %}
+{% endfor %}
