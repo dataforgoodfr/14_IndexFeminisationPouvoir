a9601a8d8d5f2033d0c651a6833710455dc6b304
Max Lever <maxdavidlever@gmail.com>
2026-04-03 17:13:30 +0200

    small updates

---
+++ nextjs/src/app/pouvoirs/local/page.tsx
@@ -0,0 +1,72 @@
+import GenderDistributionChart from "@/components/charts/GenderDistributionChart";
+import { Hero } from "@/components/Hero";
+import { FemmeElueIcon } from "@/components/icons/femme-elue";
+import { PageTitle } from "@/components/PageTitle";
+import { PouvoirFigure } from "@/components/PouvoirFigure";
+
+const local = {
+  gouvernement: 50,
+  postes_regaliens: 0,
+  cabinet_president: 35.2,
+  cabinet_premier_ministre: 42.9,
+  cabinet_gouvernement: 20,
+};
+
+export default function Page() {
+  const parite =
+    Object.values(local).reduce((acc, val) => acc + val, 0) /
+    Object.keys(local).length;
+
+  return (
+    <div className="flex flex-col mt-20 gap-8  items-center justify-center ">
+      <PageTitle title="Pouvoir local" subtitle="Le pouvoir local correspond aux élu·e·s des collectivités territoriales." />
+      <Hero>
+        <PouvoirFigure
+          valeur={parite}
+          icon={FemmeElueIcon}
+          dateMiseàJour={new Date()}
+          texte="Texte à ajouter"
+        />
+      </Hero>
+      <div className="p-8 flex flex-col gap-8">
+        <h2
+          className="text-2xl/relaxed font-bold text-center"
+          id="postes-regaliens"
+        >
+          Femmes au postes régaliens
+        </h2>
+        <GenderDistributionChart pourcentage={local.postes_regaliens} />
+        <h2
+          className="text-2xl/relaxed font-bold text-center"
+          id="gouvernement"
+        >
+          Femmes au Gouvernement
+        </h2>
+        <GenderDistributionChart pourcentage={local.gouvernement} />
+        <h2
+          className="text-2xl/relaxed font-bold text-center"
+          id="cabinet-president"
+        >
+          Cabinet du président de la république
+        </h2>
+        <GenderDistributionChart pourcentage={local.cabinet_president} />
+        <h2
+          className="text-2xl/relaxed font-bold text-center"
+          id="cabinet-premier-ministre"
+        >
+          Cabinet du premier ministre
+        </h2>
+        <GenderDistributionChart
+          pourcentage={local.cabinet_premier_ministre}
+        />
+        <h2
+          className="text-2xl/relaxed font-bold text-center"
+          id="cabinet-gouvernement"
+        >
+          Femme dirigeant un cabinet au gouvernement
+        </h2>
+        <GenderDistributionChart pourcentage={local.cabinet_gouvernement} />
+      </div>
+    </div>
+  );
+}
