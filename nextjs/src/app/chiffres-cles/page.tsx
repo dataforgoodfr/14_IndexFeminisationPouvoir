a6ba18307d0f99e9bc063d5cdc204fe35caed0ef
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2026-04-03 11:22:54 +0200

    Amélioration de la page Chiffre clés (#48)

--- nextjs/src/app/chiffres-cles/page.tsx
+++ nextjs/src/app/chiffres-cles/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 import { getTranslations } from "next-intl/server";
+import Gauge from "@/components/Gauge";
 import pouvoirData from "@/data/pouvoir.json";
-import ArcDiagramPct from "../../components/ArcDiagramPct";
 
 type Pouvoir = "executif" | "parlementaire" | "local" | "autre";
 
@@ -29,13 +29,14 @@ const PouvoirCard = async ({ pouvoir, score, evolution }: CardProps) => {
   const bgColor = evolution && evolution > 0 ? "bg-green-500" : "bg-red-500";
 
   return (
-    <div className="border-gray-200 border-2 container flex flex-col p-4">
-      <div className="text-xs">{t("title")}</div>
-      <div className="text-xs flex flex-row mt-2">
-        <div className="flex-1 text-left p-0.5 pl-2 pr-2">{score}%</div>
+    <div className="border-gray-200 border container w-50flex flex-col p-4">
+      <div className="text-[16px] text-[#697077]">{t("title")}</div>
+      <div className="flex flex-row items-center">
+        <div className="flex-1 text-[#21272A] font-bold text-2xl">{score}%</div>
         <div
-          className={`text-right rounded-lg ${bgColor} text-white p-0.5 pl-2 pr-2`}
+          className={`text-right rounded-xl ${bgColor} text-white text-[14px] py-0.5 px-2`}
         >
+          {evolution > 0 ? "+" : ""}
           {evolution}%
         </div>
       </div>
@@ -47,69 +48,84 @@ export default async function Page() {
   const data = await fetchData();
 
   return (
-    <div className="flex flex-col gap-8 items-center justify-center font-sans">
-      <div className="flex-1 flex-row max-w-6xl md:mt-20">
-        {/* First Part */}
-        <div className="flex flex-col md:flex-row w-full gap-0 relative overflow-visible">
-          {/* Left Part */}
-          <div className="flex-1 flex flex-col bg-gray-100 p-8 md:pr-30">
-            <h1 className="text-4xl/relaxed h">Présentation de l'Index</h1>
-            <div className="mt-4 text-lg">
-              L'index de Féminisation du Pouvoir mesure la représentation des
-              femmes dans les différentes instances décisionnelles depuis 2010.{" "}
-              <br />
-              Cet indicateur composite analyse la présence féminine dans les
-              organes éxecutifs, législatifs et locaux pour évaluer l'évolution
-              de la partie dans les sphères de pouvoir.
-            </div>
-            <Link
-              href="/methode"
-              className="mt-4 w-xs p-2 flex items-center justify-center"
+    <div className="flex flex-col min-h-[calc(100vh-65px)]  md:flex-row w-full gap-0 relative overflow-visible">
+      {/* Left Part */}
+      <div className="flex-1 flex flex-col gap-8 bg-[#513893] p-8 lg:px-14 lg:py-24 md:pr-30 relative">
+        <h1 className="text-5xl/tight text-[#CBB8D9] uppercase">
+          Présentation de l'Index
+        </h1>
+        <div className="mt-4 text-lg max-w-sm text-[#E9ECF7]">
+          L'index de Féminisation du Pouvoir mesure la représentation des femmes
+          dans les différentes instances décisionnelles depuis 2010. <br />
+          Cet indicateur composite analyse la présence féminine dans les organes
+          éxecutifs, législatifs et locaux pour évaluer l'évolution de la partie
+          dans les sphères de pouvoir.
+        </div>
+        <Link
+          href="/methode"
+          className="mt-4 w-xs p-2 flex items-center justify-center"
+        >
+          <div
+            className="has-tooltip absolute bottom-5 right-5 rounded-4xl w-8 h-8 flex items-center justify-center bg-gray-200"
+            aria-description="Méthodologie de calcul"
+          >
+            <span
+              className="tooltip rounded shadow-lg px-3 py-2 absolute right-full mr-2 top-1/2 -translate-y-1/2 bg-[#CBB8D9] text-white text-xs whitespace-nowrap"
+              aria-hidden={true}
             >
-              📄 En savoir plus sur le calcul de l'index
-            </Link>
+              Méthodologie de calcul
+              <svg
+                width="5"
+                height="38"
+                className="absolute right-0 top-1/2 translate-x-full -translate-y-1/2"
+                aria-hidden={true}
+              >
+                <path
+                  d="M4.29289 18.2929C4.68342 18.6834 4.68342 19.3166 4.29289 19.7071L2.16577e-06 24L1.22392e-06 14L4.29289 18.2929Z"
+                  fill="#CBB8D9"
+                />
+              </svg>
+            </span>
+            ?
           </div>
+        </Link>
+      </div>
 
-          {/* Right Part */}
-          <div className="flex-1 flex flex-col bg-gray-000 items-center justify-center relative p-8 md:pl-30">
-            <div className="flex-1 flex flex-row">
-              {/* Image overlaying between Left and Right Part */}
-              <div className="mr-4 hidden md:block absolute left-0 top-0 translate-y-1/6 -translate-x-1/2 ">
-                <Image
-                  src="/images/woman_drawing.svg"
-                  alt="divider"
-                  width={375}
-                  height={500}
-                  priority
-                />
-              </div>
-              <div className="flex-1">
-                <ArcDiagramPct
-                  value={28}
-                  radius={90}
-                  description={"Index de Féminisation du Pouvoir"}
-                />
-              </div>
-            </div>
-
-            <div className="flex-1 grid grid-cols-2 gap-5 m-5">
-              {Object.entries(data).map(([pouvoir, value]) => (
-                <PouvoirCard
-                  key={pouvoir}
-                  pouvoir={pouvoir as Pouvoir}
-                  {...value}
-                />
-              ))}
-            </div>
-
-            <Link
-              href="/evolution-donnees"
-              className="flex items-center justify-center p-2 underline"
-            >
-              Voir les évolutions
-            </Link>
+      {/* Right Part */}
+      <div className="flex-1 flex flex-col bg-gray-000 items-center justify-center relative p-8 md:pl-30">
+        <div className="flex-1 flex flex-row">
+          {/* Image overlaying between Left and Right Part */}
+          <div className="mr-4 hidden md:block absolute left-0 top-0 translate-y-1/3 -translate-x-1/2 ">
+            <Image
+              src="/images/woman_drawing.svg"
+              alt="divider"
+              aria-hidden={true}
+              width={426}
+              height={426}
+              priority
+            />
+          </div>
+          <div className="flex-1">
+            <Gauge value={28} label={"Index de\nféminisation du\npouvoir"} />
           </div>
         </div>
+
+        <div className="flex-1 grid lg:grid-cols-2 gap-5 m-5 content-center">
+          {Object.entries(data).map(([pouvoir, value]) => (
+            <PouvoirCard
+              key={pouvoir}
+              pouvoir={pouvoir as Pouvoir}
+              {...value}
+            />
+          ))}
+        </div>
+
+        <Link
+          href="/evolution-donnees"
+          className="flex items-center justify-center p-2 underline"
+        >
+          Voir les évolutions
+        </Link>
       </div>
     </div>
   );
