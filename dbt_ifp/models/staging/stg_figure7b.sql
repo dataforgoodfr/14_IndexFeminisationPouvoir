127524239582a658eca260a26dd98599f1bbd636
m-josephine <mry.josephine@gmail.com>
2026-04-07 13:09:18 +0200

    ajout de staging de la figure 7b

---
+++ dbt_ifp/models/staging/stg_figure7b.sql
@@ -0,0 +1,18 @@
+{% set start_year = 2026 %}
+{% set current_year = modules.datetime.datetime.now().year %}
+
+{% for year in range(start_year, current_year + 1) %}
+SELECT
+    {{ year }} AS annee_partition,
+    md5(personne_raw_text || personne_genre || poste_libelle) AS id,
+    personne_civilite,
+    personne_prenom,
+    personne_nom,
+    personne_genre AS genre,
+    groupe_politique_libelle,
+    poste_libelle,
+    source_url
+FROM
+    {{ ref('figure7b_' ~ year) }}
+{% if not loop.last %} UNION ALL {% endif %}
+{% endfor %}
