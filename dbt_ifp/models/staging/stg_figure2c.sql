196e9c5458994f7235adb0764307d36a68031692
Sylvio Dupin <162893500+Videobus@users.noreply.github.com>
2026-04-07 09:46:40 +0200

    Dbt refacto (#57)
    * Changement documentation seeds
    
    * Renommage + refacto des staging
    
    * Changement nom des seeds pour rajouter _2026
    
    * Rajout de l’année d’import dans les staging
    
    * Maj documentation seeds
    
    * Fix problème parsing des yml
    
    * Correction des id
    
    * Renommage champs CSP et ajout source_url
    
    ---------
    
    Co-authored-by: sylvio.dupin <sylvio.dupin@enercoop.org>
    Co-authored-by: Maud BA <79479530+maud-db@users.noreply.github.com>

---
+++ dbt_ifp/models/staging/stg_figure2c.sql
@@ -0,0 +1,21 @@
+{% set start_year = 2026 %}
+{% set current_year = modules.datetime.datetime.now().year %}
+
+{% for year in range(start_year, current_year + 1) %}
+
+select
+    {{ year }} as annee_partition,
+    md5(personne_prenom || personne_nom || zone_geographique_libelle || {{ year }}::text) as id,
+    personne_prenom as prenom_elu,
+    personne_nom as nom_elu,
+    personne_genre as genre,
+    groupe_politique_libelle,
+    poste_libelle,
+    zone_geographique_libelle,
+    zone_geographique_type,
+    source_url
+
+from {{ ref('figure2c_' ~ year) }}
+{% if not loop.last %} union all {% endif %}
+{% endfor %}
+
