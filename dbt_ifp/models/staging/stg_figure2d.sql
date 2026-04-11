53d9cec4b63a59605984f912ef1243f33d2c36c1
Sylvio Dupin <162893500+Videobus@users.noreply.github.com>
2026-04-11 22:02:28 +0200

    Ajout du staging dbt de la figure 2d (#72)
    Co-authored-by: sylvio.dupin <sylvio.dupin@enercoop.org>

---
+++ dbt_ifp/models/staging/stg_figure2d.sql
@@ -0,0 +1,20 @@
+-- stg_figure6b.sql
+{% set start_year = 2026 %}
+{% set current_year = modules.datetime.datetime.now().year %}
+
+{% for year in range(start_year, current_year + 1) %}
+SELECT
+    {{ year }} AS annee_partition,
+    md5(personne_raw_text || zone_geographique_libelle) AS id,
+    personne_civilite,
+    personne_prenom,
+    personne_nom,
+    personne_genre AS genre,
+    groupe_politique_libelle,
+    poste_libelle,
+    zone_geographique_libelle,
+    zone_geographique_type
+FROM
+    {{ ref('figure2d_' ~ year) }}
+{% if not loop.last %} UNION ALL {% endif %}
+{% endfor %}
