53ab0c4a455d8d353e32cac325a6f9266c47784e
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2026-04-16 17:37:03 +0200

    update hero

--- nextjs/src/components/Hero.tsx
+++ nextjs/src/components/Hero.tsx
@@ -5,6 +5,7 @@ export type HeroProps = {
 };
 export const Hero = ({ children }: HeroProps) => (
   <div className="w-full h-180 lg:h-120 flex items-center justify-center relative bg-svg-purple">
+    <span className="absolute top-0 border-l-24 border-l-transparent border-r-24 border-r-transparent border-t-24 border-t-white" />
     <div className="relative">{children}</div>
   </div>
 );
