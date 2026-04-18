095b5456f6135e4da0222756bc6fd34a6c6a12d9
Max Lever <maxdavidlever@gmail.com>
2026-04-19 00:21:20 +0200

    fix: prevent info icon from shrinking in InfoBox (add shrink-0)

--- nextjs/src/components/InfoBox.tsx
+++ nextjs/src/components/InfoBox.tsx
@@ -7,7 +7,7 @@ type InfoBoxProps = {
 export const InfoBox = ({ children }: InfoBoxProps) => {
   return (
     <div className="flex items-start gap-4 bg-foundations-violet-tres-clair p-5">
-      <InfoIcon className="w-12 h-12" />
+      <InfoIcon className="w-12 h-12 shrink-0" />
       <div>{children}</div>
     </div>
   );
