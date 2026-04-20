00a536edb236ec82e6bf2672dc0d2f7667488c88
PMoneyron <pierre.moneyron@gadz.org>
2026-04-20 18:26:55 +0200

    first version of the 404 page (#109)

--- nextjs/src/app/not-found.tsx
+++ nextjs/src/app/not-found.tsx
@@ -1,9 +1,34 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function NotFound() {
   return (
-    <div className="flex items-center justify-center">
-      <main className="flex w-full max-w-6xl flex-col items-center justify-center py-32 md:px-16 ">
-        <h1 className="text-5xl text-center">Page non trouvée</h1>
-      </main>
+    <div className="flex flex-1 flex-col items-center gap-12 py-[50px]">
+      <div className="flex flex-1 flex-col gap-6.5 items-center w-full max-w-[590px] h-auto">
+        <Image
+          src={`${process.env.NEXT_PUBLIC_BASE_PATH ?? ""}/images/inequal.svg`}
+          alt="Inequal image"
+          width={410}
+          height={384}
+          className="w-full max-w-[410px] h-auto"
+          priority
+        />
+        <div className="divider"> </div>
+        <h1 className="header-h1 text-center text-foundations-violet-principal">
+          la page demandée n'existe pas
+        </h1>
+        <div className="flex flex-col items-center h-full max-h-[341px]">
+          <p className="font-tstar text-foundations-violet-principal text-[214px]/[1.25] font-bold">
+            404
+          </p>
+        </div>
+        <Link
+          href="/"
+          className="button button-primary font-tstar text-[20px] font-bold uppercase;"
+        >
+          retour à l'accueil
+        </Link>
+      </div>
     </div>
   );
 }
