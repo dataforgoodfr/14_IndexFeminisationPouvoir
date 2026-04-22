053d48f194a6bccca6c989cd51a0d3c9897c32d2
PMoneyron <pierre.moneyron@gadz.org>
2026-04-22 19:13:23 +0200

    Nouvelle page Mentions legales (#114)

---
+++ nextjs/src/app/mentions/page.tsx
@@ -0,0 +1,94 @@
+export default function Page() {
+  return (
+    <div className="flex flex-1 flex-col items-center gap-[65.5px] pt-[50px] pb-[90px]">
+      <div className="flex flex-col items-center gap-[10px]">
+        <h1 className="header-h1 text-center text-foundations-violet-principal uppercase">
+          mentions légales <br /> & politique de confidentialité
+        </h1>
+        <div className="divider"> </div>
+      </div>
+      <div className="flex flex-col items-start bg-foundations-violet-tres-clair px-[31px] py-[29px] max-w-[804px] w-full">
+        <div>
+          <p className="body1-regular">
+            <b>Ce site est la propriété de :</b>
+            <br />
+            Oxfam France - 62 bis, avenue Parmentier - 75011 Paris, France
+            <br />
+            <br />
+            <b>Directeur.rice de la publication</b> : <b>XXXXXXX</b>
+            <br />
+            <b>Réalisation</b> : Data For Good
+            <br />
+            <b>Hébergement</b> : <b>XXXXXXX</b>
+            <br />
+            <b>Propriété intellectuelle - Crédits Photos</b> : Oxfam
+            <br />
+            <br />
+            <b>
+              Politique de confidentialité et Protection des données
+              personnelles
+            </b>
+            <br />
+            Le Propriétaire du site se réserve le droit de collecter les
+            informations nominatives et les données personnelles vous concernant
+            et s'engage à traiter ces données conformément à la réglementation
+            en vigueur en matière de protection des données personnelles
+            (notamment le RGPD).
+            <br />
+            <br />
+            Conformément à la loi du 6 janvier 1978, vous disposez d'un droit
+            d'accès, de rectification et d'opposition aux informations
+            nominatives et aux données personnelles vous concernant, en
+            contactant notre DPO à l'adresse : <b>Adresse E-mail</b>
+            <br />
+            Si vous estimez, après avoir contacté le Propriétaire du site, que
+            vos droits « Informatique et Libertés » ne sont pas respectés, vous
+            pouvez adresser une réclamation en ligne à la CNIL.
+            <br />
+            <br />
+            <b>Cookies</b>
+            <br />
+            Un cookie est un petit fichier contenant un ensemble d'informations
+            qu'un site Internet peut transmettre à votre navigateur durant votre
+            navigation. Il sera conservé pendant une certaine durée sur votre
+            ordinateur, et sera utilisé chaque fois que vous vous connecterez à
+            nouveau à ce site. Un cookie peut avoir de multiples usages :
+            mémoriser votre identifiant client sur un site e-commerce, le
+            contenu de votre panier d'achat, enregistrer votre navigation à des
+            fins statistiques ou publicitaires, etc.
+            <br />
+            Différents cookies sont utilisés sur le site Adresse du site pour
+            mesurer l'audience et intégrer des services permettant d'améliorer
+            l'interactivité du site et sont ainsi sous la responsabilité du
+            Propriétaire du site.
+            <br />
+            <br />
+            <b>1 type de cookie est déposé sur le site ADRESSE DU SITE</b>
+            <br />
+            <br />
+            <b>
+              Cookies de mesure d'audience et cookies tiers destinés à améliorer
+              l'interactivité du site.
+            </b>
+            <br />
+            Les sites s'appuient les cookies tiers suivants (directement déposés
+            par les services concernés) :
+            <br />
+            <b>- A compléter par nos soins</b>
+            <br />
+            Via ces cookies, ces services collectent et utilisent vos données de
+            navigation pour leur propre compte dans le but de de proposer des
+            contenus et de la publicité ciblée. 
+            <br />
+            Vous pouvez activer/désactiver leur utilisation via le lien
+            disponible en bas de chaque page.
+            <br />
+            <b>Cookies de mesure d'audience</b>
+            <br />
+            <b>Nom du cookie-Finalité-Durée de conservation</b>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
