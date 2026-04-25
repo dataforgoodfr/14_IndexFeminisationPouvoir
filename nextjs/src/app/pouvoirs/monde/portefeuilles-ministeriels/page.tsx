8f9b9e2d8c2960be5a4ef3c499314eb9d006a02a
Max Lever <maxdavidlever@gmail.com>
2026-04-25 22:45:24 +0200

    start portefeuilles

--- nextjs/src/app/pouvoirs/monde/portefeuilles-ministeriels/page.tsx
+++ nextjs/src/app/pouvoirs/monde/portefeuilles-ministeriels/page.tsx
@@ -1,17 +1,87 @@
+import { InfoBox } from '@/components/InfoBox';
+import { LiensCTA } from '@/components/LiensCTA';
 import { ShortDate } from "@/components/ShortDate";
+import { Standings } from '@/components/Standings';
 
 export default function PortefeuillesMinisterielsPage() {
   return (
-    <div className="flex flex-col items-center gap-12 py-12 px-12 max-w-7xl mx-auto w-full">
-      <div className="flex flex-col items-center gap-3">
-        <h2 className="header-h1 text-foundations-violet-principal text-center">
-          Portefeuilles ministériels
-        </h2>
-        <p className="body2-regular text-black">
-          Dernière mise à jour : {/* TODO: */}
-          <ShortDate date={new Date()} />
-        </p>
-        <div className="bg-foundations-violet-clair h-1.5 w-15 rounded-full" />
+    <div className="flex flex-col items-center gap-12 py-12 px-12 max-w-6xl mx-auto w-full">
+      <div className="flex flex-col md:flex-row gap-x-10 items-center px-5">
+        <div className="flex-4 md:flex-1">
+          <h2 className="header-h3 text-foundations-violet-principal">
+            Le top 5 des portefeuilles ministériels occupés par des femmes dans le monde
+          </h2>
+          <div className="h-5" />
+          <Standings data={[
+            {
+              label: "Femmes et égalité des sexes ",
+              percentage: 14,
+              evolution: -100
+            },
+            {
+              label: "Affaires familiales et enfance",
+              percentage: 14,
+              evolution: 100
+            },
+            {
+              label: "Inclusion sociale et développement",
+              percentage: 14,
+              evolution: -100
+            },
+            {
+              label: "Affaire sociale",
+              percentage: 14,
+              evolution: -100
+            },
+            {
+              label: "Protection sociale et sécurité sociale",
+              percentage: 14,
+              evolution: 100
+            },
+          ]} />
+          <div className="h-5" />
+
+          <h2 className="header-h3 text-foundations-violet-principal">
+            Le top 5 des portefeuilles ministériels les moins féminisés
+          </h2>
+          <div className="h-5" />
+          <Standings data={[
+            {
+              label: "Les affaires religieuses",
+              percentage: 14,
+              evolution: -100
+            },
+            {
+              label: "Energie, combustibles naturels et mines",
+              percentage: 14,
+              evolution: 100
+            },
+            {
+              label: "Transport",
+              percentage: 14,
+              evolution: -100
+            },
+            {
+              label: "Défense",
+              percentage: 14,
+              evolution: -100
+            },
+            {
+              label: "Affaires intérieures",
+              percentage: 14,
+              evolution: 100
+            },
+          ]} />
+        </div>
+        <div className="h-10 md:hidden" />
+        <div className="flex-1 md:flex-1 flex flex-row gap-x-5 md:gap-x-15">
+          <LiensCTA />
+          <InfoBox>
+            L’attribution des portefeuilles ministériels reste genrée.
+
+            En effet, les femmes ont particulièrement la charge de thématiques liées aux droits des femmes et au « care ».
+          </InfoBox>
+        </div>
       </div>
     </div>
   );
