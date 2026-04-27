a22c076b71bf5b1b385c6969f84c0df4df5e43bd
m-josephine <mry.josephine@gmail.com>
2026-04-27 12:16:49 +0200

    Modification orthographe champ nb_personnes_attendu

--- dbt_ifp/models/marts/calculated_figures_2025.sql
+++ dbt_ifp/models/marts/calculated_figures_2025.sql
@@ -4,5 +4,5 @@ SELECT
     institution_type as institution_type,
     nom_figure as figure_nom,
     CAST(REPLACE(REPLACE(reference_rapport_2025::TEXT, ',', '.'), '%', '') AS FLOAT) as pct_femmes,
-    nb_personnes_attendu as nb_personnes_attendues
+    nb_personnes_attendu as nb_personnes_attendu
 FROM {{ ref('figures_2025') }}
