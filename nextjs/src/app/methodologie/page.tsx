f379e8735e6b67f3aea547326e60115a6e7e6547
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2026-05-05 20:20:33 +0200

    page methodo

--- nextjs/src/app/methodologie/page.tsx
+++ nextjs/src/app/methodologie/page.tsx
@@ -1,8 +1,44 @@
+import { LoupeIcon } from "@/components/icons/loupe";
+import { NormalisationIcon } from "@/components/icons/normalisation";
+
+type EtapeProps = {
+  title: string;
+  icon: React.ReactNode;
+  description: React.ReactNode;
+  order: number;
+};
+const Etape = ({ title, icon, description, order }: EtapeProps) => (
+  <div className="flex flex-row items-center gap-4 p-4 border border-gray-200">
+    {icon}
+    <div className="flex flex-col">
+      <h3 className="header-h3">
+        Étape {order} : {title}
+      </h3>
+      <p className="body1-medium text-foundations-gris-fonce">{description}</p>
+    </div>
+  </div>
+);
 export default function Page() {
   return (
-    <div>
-      <h1>Méthode</h1>
-      <p>Contenu de la page de méthode.</p>
+    <div className="lg:w-260 flex flex-col gap-10">
+      <Etape
+        order={1}
+        title="Collecte de données"
+        icon={<LoupeIcon />}
+        description="Nous collectons des données de sources variées afin d'assurer leur fiabilité."
+      />
+      <Etape
+        order={2}
+        title="Normalisation"
+        icon={<NormalisationIcon />}
+        description="Les données collectées sont normalisées pour garantir leur cohérence."
+      />
+      <Etape
+        order={3}
+        title="Calcul de la tendance"
+        icon={<LoupeIcon />}
+        description="Les données collectées sont normalisées pour garantir leur cohérence."
+      />
     </div>
   );
 }
