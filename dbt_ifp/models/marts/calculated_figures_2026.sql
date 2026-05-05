71e230eedd976d190491c33d3fced30a0782ed04
m-josephine <mry.josephine@gmail.com>
2026-05-05 18:14:25 +0200

    Ajout de la table calculee des figures 2026

---
+++ dbt_ifp/models/marts/calculated_figures_2026.sql
@@ -0,0 +1,20 @@
+with figures_2026 as 
+(select
+	pouvoir_type,
+	institution_type,
+	figure,
+	ROUND(SUM(case when personne_genre = 'F' then 1.0 else 0 END) / NULLIF(COUNT(*), 0)*100, 1) as pct_femmes
+from {{ ref('int_concat_figures_2026') }}
+group by 1,2,3
+
+)
+
+select
+	a.pouvoir_type,
+	a.institution_type,
+	a.figure,
+	a.pct_femmes,
+	b.pct_femmes as pct_femmes_2025,
+	(a.pct_femmes - b.pct_femmes) as evolution
+from figures_2026 a
+left join {{ ref('calculated_figures_2025') }} b on a.figure = b.figure
