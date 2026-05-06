9cbacff3747273fdfb613ddc8a3b8b5005e52e60
M-Josephine <66204884+M-Josephine@users.noreply.github.com>
2026-05-06 09:50:35 +0200

    Ajout de la table calculee des figures 2026 (#153)
    * Ajout de la table calculee des figures 2026
    
    * Update dbt_ifp/models/marts/calculated_figures_2026.sql
    
    Co-authored-by: Sylvio Dupin <162893500+Videobus@users.noreply.github.com>
    
    * Update dbt_ifp/models/marts/calculated_figures_2026.sql
    
    Co-authored-by: Sylvio Dupin <162893500+Videobus@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Sylvio Dupin <162893500+Videobus@users.noreply.github.com>

--- dbt_ifp/models/marts/calculated_figures_2025.sql
+++ dbt_ifp/models/marts/calculated_figures_2025.sql
@@ -2,7 +2,7 @@ SELECT
     type_pouvoir as pouvoir_type,
     indicateur as indicateur,
     institution_type as institution_type,
-    nom_figure as figure_nom,
+    lower(replace(nom_figure, ' ','')) as figure,
     CAST(REPLACE(REPLACE(reference_rapport_2025::TEXT, ',', '.'), '%', '') AS FLOAT) as pct_femmes,
     nb_personnes_attendu as nb_personnes_attendu
 FROM {{ ref('figures_2025') }}
